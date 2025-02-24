6f5e546c010493409afb4472eec6ca520fed0f8b
secnops <secnops0@gmail.com>
2025-02-24 14:10:27 +0000

    Add server to handle incoming requests

---
+++ cmd/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"io"
+	"log"
+	"log/slog"
+	"net/http"
+
+	r "github.com/secnops/proxit/requester"
+)
+
+func main() {
+	// Hello world, the web server
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		// we will proxy everything except the port, the remote address
+		// and the path due to framework stuff
+		// if the requests sends a remote-address we will point the proxied request to there
+		// else 127.0.0.1 it is
+		pathToRequest := req.Header.Get("path")
+		port := req.Header.Get("port")
+		if /*pathToRequest == "" ||*/ port == "" {
+			io.WriteString(w, `
+			The request should be something like:
+			GET /PATH HTTP/1.1
+			Host: host
+			Port: [port to connect to]
+			Remote-address: [if you do not want to connect to a local service]
+			Tls:  [if the other service is running over Tls]
+			`)
+			return
+		}
+		remoteAddr := req.Header.Get("remote-address")
+		tls := req.Header.Get("tls")
+		schema := "http://"
+		if remoteAddr == "" {
+			remoteAddr = "127.0.0.1"
+		}
+		if tls != "" {
+			schema = "https://"
+		}
+
+		url := schema + remoteAddr + ":" + port + pathToRequest
+
+		slog.Info("Request info", "Method", req.Method, "Url", url)
+		i := r.Request(req.Method, url, "", map[string]string{})
+		io.WriteString(w, i)
+		return
+	}
+
+	http.HandleFunc("/", helloHandler)
+	log.Fatal(http.ListenAndServe(":8000", nil))
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/secnops/proxit
+
+go 1.23.5
